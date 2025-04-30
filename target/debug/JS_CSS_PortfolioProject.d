bee5d5d94893511e65d1dc2e8708672ebdd4e962
dericko681 <abonghoderick@gmail.com>
2025-04-30 21:21:49 +0100

    my porfolio

---
+++ target/debug/JS_CSS_PortfolioProject.d
@@ -0,0 +1,1 @@
+/home/kunga/projects/javascript_projects/portfolio/target/debug/JS_CSS_PortfolioProject: /home/kunga/projects/javascript_projects/portfolio/src/main.rs
---
+++ target/debug/deps/JS_CSS_PortfolioProject-bfc03b14d4253469.d
@@ -0,0 +1,5 @@
+/home/kunga/projects/javascript_projects/portfolio/target/debug/deps/libJS_CSS_PortfolioProject-bfc03b14d4253469.rmeta: src/main.rs
+
+/home/kunga/projects/javascript_projects/portfolio/target/debug/deps/JS_CSS_PortfolioProject-bfc03b14d4253469.d: src/main.rs
+
+src/main.rs:
